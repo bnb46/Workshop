adad1b2b159e11a07c07e0841d536a46d8ef713e
bnb46 <112518648+bnb46@users.noreply.github.com>
2025-10-23 00:58:30 -0500

    Update Code_For_Pi5.py

--- LED_CONTROL/Code_For_Pi5.py
+++ LED_CONTROL/Code_For_Pi5.py
@@ -1,10 +1,13 @@
-from gpiozero import LED
-from time import sleep
+from gpiozero import LED    # Import the GPIO library
+from time import sleep      # Import the time library
 
-led = LED(14)
+led = LED(14)    # Tell python that your LED is on pin 14
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+while True: # While loop
+    led.on()            # Turn the led ON
+    print("LED is ON")  # Display confirmation message
+    sleep(1)            # "sleep" (wait) for 1 second
+    
+    led.off()           # Turn the led OFF
+    print("LED is OFF") # Display confirmation message
+    sleep(1)            # "sleep" (wait) for 1 second
